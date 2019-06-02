b5cb2d567880c89d2e6df28929714a248b1db900
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-06-02 12:01:04 +0200

    hibernate

---
+++ src/main/java/pl/sda/rest/model/Role.java
@@ -0,0 +1,34 @@
+package pl.sda.rest.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String role_name;
+
+
+
+    public Role(String role_name) {
+        this.role_name = role_name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+}
--- src/main/java/pl/sda/rest/model/User.java
+++ src/main/java/pl/sda/rest/model/User.java
@@ -1,15 +1,19 @@
 package pl.sda.rest.model;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
 public class User {
-    String login;
-    String password;
-    LocalDateTime registration_date;
-    Boolean active;
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+    private String login;
+    private String password;
+    private LocalDateTime registration_date = LocalDateTime.now();
+    private boolean active = false;
 
     public User(String login, String password, LocalDateTime registration_date, Boolean active) {
         this.login = login;
@@ -49,5 +53,16 @@ public class User {
     public void setActive(Boolean active) {
         this.active = active;
     }
+
+    @ManyToMany (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+@JoinTable(
+        name = "user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+)
+
+    private Set<Role> roles = new HashSet<>();
+
 }
+
 
